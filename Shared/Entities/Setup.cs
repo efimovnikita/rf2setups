cd0738b9758f3e527a8f5dccb8999bed7a5f5010
Nikita Efimov <maskedball@192.168.0.12>
2020-09-07 23:07:11 +0300

    add new setup classes

--- Shared/Entities/Setup.cs
+++ Shared/Entities/Setup.cs
@@ -5,14 +5,58 @@ namespace rf2setups.Shared.Entities
 {
     public class Setup
     {
-        [Required]
         public Guid Id { get; set; }
-        [Required]
         public string Name { get; set; }
         public DateTime Date { get; set; }
-        [Required]
         public Guid Car { get; set; }
-        [Required]
         public Guid Track { get; set; }
+        public TiresSetups Tyres { get; set; }
+        public BrakesSetups Brakes { get; set; }
+        public ChassisSetups Chassis { get; set; }
+        public SuspensionSetups Suspension { get; set; }
+        public AerodynamicsSetups Aerodynamics { get; set; }
+        public ElectronicsSetups Electronics { get; set; }
+    }
+
+    public class TiresSetups
+    {
+        public enum Compound
+        {
+            Soft, 
+            Medium,
+            Hard
+        }
+
+        public double FrontLeftCamber { get; set; }
+        public double FrontRightCamber { get; set; }
+        public double RearLeftCamber { get; set; }
+        public double RearRightCamber { get; set; }
+        public Compound FrontCompound { get; set; }
+        public Compound RearCompound { get; set; }
+    }
+
+    public class BrakesSetups
+    {
+        
+    }
+
+    public class ChassisSetups
+    {
+        
+    }
+
+    public class SuspensionSetups
+    {
+        
+    }
+
+    public class AerodynamicsSetups
+    {
+        
+    }
+
+    public class ElectronicsSetups
+    {
+        
     }
 }
