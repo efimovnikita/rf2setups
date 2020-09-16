506bdd1f245b30d570b263e600d214f0d0639e2a
Nikita Efimov <maskedballmail@gmail.com>
2020-09-16 21:21:13 +0300

    Fill add setup page

--- Shared/AppData.cs
+++ Shared/AppData.cs
@@ -7,8 +7,8 @@ namespace rf2setups.Shared
 {
     public class AppData
     {
-        //public string API { get; set; } = "http://localhost:5001/rf2setups/us-central1/api";
-        public string API { get; set; } = "https://us-central1-rf2setups.cloudfunctions.net/api";
+        public string API { get; set; } = "http://localhost:5001/rf2setups/us-central1/api";
+        //public string API { get; set; } = "https://us-central1-rf2setups.cloudfunctions.net/api";
         public List<Car> Cars { get; set; }
         public List<Track> Tracks { get; set; }
         public List<Setup> Setups { get; set; }
--- Shared/Entities/Setup.cs
+++ Shared/Entities/Setup.cs
@@ -23,72 +23,162 @@ namespace rf2setups.Shared.Entities
 
     public class TiresSetups
     {
+        [Required]
+        [Range(-10.0, 10.0, ErrorMessage = "Please enter a number between -10 and 10")]
         public double FrontLeftCamber { get; set; } = -3.8;
+        [Required]
+        [Range(-10.0, 10.0, ErrorMessage = "Please enter a number between -10 and 10")]
         public double FrontRightCamber { get; set; } = -3.8;
+        [Required]
+        [Range(-10.0, 10.0, ErrorMessage = "Please enter a number between -10 and 10")]
         public double RearLeftCamber { get; set; } = -3.8;
+        [Required]
+        [Range(-10.0, 10.0, ErrorMessage = "Please enter a number between -10 and 10")]
         public double RearRightCamber { get; set; } = -3.8;
+        [Required]
         public string FrontCompound { get; set; } = "Soft";
+        [Required]
         public string RearCompound { get; set; } = "Soft";
     }
 
     public class BrakesSetups
     {
+        [Required]
+        [Range(0.0, 100.0, ErrorMessage = "Please enter a number between 0 and 100")]
         public double FrontBrakeBias { get; set; } = 54.0;
+        [Required]
+        [Range(0.0, 100.0, ErrorMessage = "Please enter a number between 0 and 100")]
         public double RearBrakeBias { get; set; } = 46.0;
+        [Required]
+        [Range(0, 100, ErrorMessage = "Please enter a number between 0 and 100")]
         public int MaxPedalForce { get; set; } = 80;
     }
 
     public class SuspensionSetups
     {
+        [Required]
+        [Range(0, 300, ErrorMessage = "Please enter a number between 0 and 300")]
         public int FrontSpringRateLeft { get; set; } = 160;
+        [Required]
+        [Range(0, 300, ErrorMessage = "Please enter a number between 0 and 300")]
         public int FrontSpringRateRight { get; set; } = 160;
+        [Required]
+        [Range(0, 300, ErrorMessage = "Please enter a number between 0 and 300")]
         public int RearSpringRateLeft { get; set; } = 160;
+        [Required]
+        [Range(0, 300, ErrorMessage = "Please enter a number between 0 and 300")]
         public int RearSpringRateRight { get; set; } = 160;
+        [Required]
+        [Range(0, 10, ErrorMessage = "Please enter a number between 0 and 10")]
         public int FrontSlowBumpLeft { get; set; } = 8;
+        [Required]
+        [Range(0, 10, ErrorMessage = "Please enter a number between 0 and 10")]
         public int FrontSlowBumpRight { get; set; } = 8;
+        [Required]
+        [Range(0, 10, ErrorMessage = "Please enter a number between 0 and 10")]
         public int RearSlowBumpLeft { get; set; } = 8;
+        [Required]
+        [Range(0, 10, ErrorMessage = "Please enter a number between 0 and 10")]
         public int RearSlowBumpRight { get; set; } = 8;
+        [Required]
+        [Range(0, 10, ErrorMessage = "Please enter a number between 0 and 10")]
         public int FrontSlowReboundLeft { get; set; } = 7;
+        [Required]
+        [Range(0, 10, ErrorMessage = "Please enter a number between 0 and 10")]
         public int FrontSlowReboundRight { get; set; } = 7;
+        [Required]
+        [Range(0, 10, ErrorMessage = "Please enter a number between 0 and 10")]
         public int RearSlowReboundLeft { get; set; } = 7;
+        [Required]
+        [Range(0, 10, ErrorMessage = "Please enter a number between 0 and 10")]
         public int RearSlowReboundRight { get; set; } = 7;
+        [Required]
+        [Range(0, 10, ErrorMessage = "Please enter a number between 0 and 10")]
         public int FrontFastBumpLeft { get; set; } = 5;
+        [Required]
+        [Range(0, 10, ErrorMessage = "Please enter a number between 0 and 10")]
         public int FrontFastBumpRight { get; set; } = 5;
+        [Required]
+        [Range(0, 10, ErrorMessage = "Please enter a number between 0 and 10")]
         public int RearFastBumpLeft { get; set; } = 5;
+        [Required]
+        [Range(0, 10, ErrorMessage = "Please enter a number between 0 and 10")]
         public int RearFastBumpRight { get; set; } = 5;
+        [Required]
+        [Range(0, 10, ErrorMessage = "Please enter a number between 0 and 10")]
         public int FrontFastReboundLeft { get; set; } = 4;
+        [Required]
+        [Range(0, 10, ErrorMessage = "Please enter a number between 0 and 10")]
         public int FrontFastReboundRight { get; set; } = 4;
+        [Required]
+        [Range(0, 10, ErrorMessage = "Please enter a number between 0 and 10")]
         public int RearFastReboundLeft { get; set; } = 4;
+        [Required]
+        [Range(0, 10, ErrorMessage = "Please enter a number between 0 and 10")]
         public int RearFastReboundRight { get; set; } = 4;
+        [Required]
+        [Range(0.0, 10.0, ErrorMessage = "Please enter a number between 0 and 10")]
         public double FrontPackersLeft { get; set; } = 2.8;
+        [Required]
+        [Range(0.0, 10.0, ErrorMessage = "Please enter a number between 0 and 10")]
         public double FrontPackersRight { get; set; } = 2.8;
+        [Required]
+        [Range(0.0, 10.0, ErrorMessage = "Please enter a number between 0 and 10")]
         public double RearPackersLeft { get; set; } = 2.8;
+        [Required]
+        [Range(0.0, 10.0, ErrorMessage = "Please enter a number between 0 and 10")]
         public double RearPackersRight { get; set; } = 2.8;
+        [Required]
+        [Range(0.0, 10.0, ErrorMessage = "Please enter a number between 0 and 10")]
         public double FrontRideHeightLeft { get; set; } = 5.8;
+        [Required]
+        [Range(0.0, 10.0, ErrorMessage = "Please enter a number between 0 and 10")]
         public double FrontRideHeightRight { get; set; } = 5.8;
+        [Required]
+        [Range(0.0, 10.0, ErrorMessage = "Please enter a number between 0 and 10")]
         public double RearRideHeightLeft { get; set; } = 5.8;
+        [Required]
+        [Range(0.0, 10.0, ErrorMessage = "Please enter a number between 0 and 10")]
         public double RearRideHeightRight { get; set; } = 5.8;
+        [Required]
+        [Range(0, 10, ErrorMessage = "Please enter a number between 0 and 10")]
         public int FrontAntisway { get; set; } = 2;
+        [Required]
+        [Range(0, 10, ErrorMessage = "Please enter a number between 0 and 10")]
         public int RearAntisway { get; set; } = 2;
+        [Required]
+        [Range(-2.0, 2.0, ErrorMessage = "Please enter a number between -2 and 2")]
         public double FrontToeIn { get; set; } = -0.12;
+        [Required]
+        [Range(-2.0, 2.0, ErrorMessage = "Please enter a number between -2 and 2")]
         public double RearToeIn { get; set; } = -0.12;
     }
 
     public class AerodynamicsSetups
     {
+        [Required]
+        [Range(0, 12, ErrorMessage = "Please enter a number between 0 and 12")]
         public int FrontWing { get; set; } = 4;
+        [Required]
+        [Range(0, 12, ErrorMessage = "Please enter a number between 0 and 12")]
         public int RearWing { get; set; } = 4;
 
     }
 
     public class ElectronicsSetups
     {
+        [Required]
+        [Range(0, 10, ErrorMessage = "Please enter a number between 0 and 10")]
         public int TractionControl { get; set; } = 5;
+        [Required]
+        [Range(0, 10, ErrorMessage = "Please enter a number between 0 and 10")]
         public int ABS { get; set; } = 2;
     }
 
     public class DriveTrainSetups
     {
+        [Required]
+        [Range(0, 200, ErrorMessage = "Please enter a number between 0 and 200")]
         public int Preload { get; set; } = 140;
     }
 }
